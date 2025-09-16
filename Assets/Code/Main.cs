a4c4096ec095bb8527b4020979f6f1753e1ebf1d
iarrans <isabelarrans@gmail.com>
2025-09-16 19:25:05 +0200

    countdown (still no score)

--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -1,20 +1,34 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
     public int score = 0;
+    public bool playing= false;
     public ObstacleGenerator obstacleGenerator = null;
+    public float remaining = 60f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(CountdownCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CountdownCoroutine()
     {
+        playing = true;
+        while (remaining > 0)
+        {
+            if (playing){
+            Debug.Log("Remaining: " + remaining);
+            yield return new WaitForSeconds(1f);
+            remaining--;
+            }
+        }
         
+        SceneManager.LoadScene("EndScene");
     }
+
 
     public void IncreaseCounter()
     {
